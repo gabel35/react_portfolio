0ee65020cb3da7a1f9801611b4cb2f93cd952b22
gabel35 <gabrieljose3135@gmail.com>
2021-04-07 02:50:40 -0400

    basic pathing

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div
+      className="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h2 class="display-4" style="font-size: large;">Welcome to </h2>
+        <h1 class="display-4">Gabriel J. Sanchez's Portfolio!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
